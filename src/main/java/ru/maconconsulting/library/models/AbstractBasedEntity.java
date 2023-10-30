d6acd9b7f85e0cc244adc3bf77c4d85925dbd6c5
MaximKiel <loup.du.nord@yandex.ru>
2023-10-30 20:56:12 +0300

    Implement basic methods into model classes

--- src/main/java/ru/maconconsulting/library/models/AbstractBasedEntity.java
+++ src/main/java/ru/maconconsulting/library/models/AbstractBasedEntity.java
@@ -7,4 +7,36 @@ public abstract class AbstractBasedEntity {
     private Integer id;
 
     private LocalDateTime createdAt;
+
+    public AbstractBasedEntity() {
+    }
+
+    public AbstractBasedEntity(Integer id, LocalDateTime createdAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractBasedEntity{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
--- src/main/java/ru/maconconsulting/library/models/Project.java
+++ src/main/java/ru/maconconsulting/library/models/Project.java
@@ -19,4 +19,96 @@ public class Project extends AbstractBasedEntity {
     private String type;
 
     private Integer year;
+
+    public Project() {
+    }
+
+    public Project(Integer number, String title, List<String> countries, List<String> regions, List<String> towns, List<String> segments, String type, Integer year) {
+        this.number = number;
+        this.title = title;
+        this.countries = countries;
+        this.regions = regions;
+        this.towns = towns;
+        this.segments = segments;
+        this.type = type;
+        this.year = year;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<String> countries) {
+        this.countries = countries;
+    }
+
+    public List<String> getRegions() {
+        return regions;
+    }
+
+    public void setRegions(List<String> regions) {
+        this.regions = regions;
+    }
+
+    public List<String> getTowns() {
+        return towns;
+    }
+
+    public void setTowns(List<String> towns) {
+        this.towns = towns;
+    }
+
+    public List<String> getSegments() {
+        return segments;
+    }
+
+    public void setSegments(List<String> segments) {
+        this.segments = segments;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "number=" + number +
+                ", title='" + title + '\'' +
+                ", countries=" + countries +
+                ", regions=" + regions +
+                ", towns=" + towns +
+                ", segments=" + segments +
+                ", type='" + type + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
