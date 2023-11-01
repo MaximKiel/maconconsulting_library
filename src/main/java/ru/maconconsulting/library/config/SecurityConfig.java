4e23ee4bef7c5fe9593856fb65a6b62e891357d7
MaximKiel <loup.du.nord@yandex.ru>
2023-11-01 20:27:54 +0300

    Comment Security classes

--- src/main/java/ru/maconconsulting/library/config/SecurityConfig.java
+++ src/main/java/ru/maconconsulting/library/config/SecurityConfig.java
@@ -1,58 +1,58 @@
-package ru.maconconsulting.library.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import ru.maconconsulting.library.services.MaconUserDetailsService;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-public class SecurityConfig {
-
-    private final MaconUserDetailsService service;
-
-    @Autowired
-    public SecurityConfig(MaconUserDetailsService service) {
-        this.service = service;
-    }
-
-//    TODO: configure filter chains
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
-                                .anyRequest().hasAnyRole("USER", "MANAGER", "ADMIN")
-                )
-                .formLogin((formLogin) ->
-                        formLogin
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                .loginPage("/auth/login")
-                                .defaultSuccessUrl("/hello", true)
-                                .failureUrl("/auth/login?error")
-                                .loginProcessingUrl("/process_login")
-                )
-                .logout((logout) ->
-                        logout.logoutUrl("/logout")
-                                .logoutSuccessUrl("/auth/login"));
-        return http.build();
-    }
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(service).passwordEncoder(getPasswordEncoder());
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package ru.maconconsulting.library.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import ru.maconconsulting.library.services.MaconUserDetailsService;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class SecurityConfig {
+//
+//    private final MaconUserDetailsService service;
+//
+//    @Autowired
+//    public SecurityConfig(MaconUserDetailsService service) {
+//        this.service = service;
+//    }
+//
+////    TODO: configure filter chains
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests((authorizeRequests) ->
+//                        authorizeRequests
+//                                .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+//                                .anyRequest().hasAnyRole("USER", "MANAGER", "ADMIN")
+//                )
+//                .formLogin((formLogin) ->
+//                        formLogin
+//                                .usernameParameter("username")
+//                                .passwordParameter("password")
+//                                .loginPage("/auth/login")
+//                                .defaultSuccessUrl("/hello", true)
+//                                .failureUrl("/auth/login?error")
+//                                .loginProcessingUrl("/process_login")
+//                )
+//                .logout((logout) ->
+//                        logout.logoutUrl("/logout")
+//                                .logoutSuccessUrl("/auth/login"));
+//        return http.build();
+//    }
+//
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(service).passwordEncoder(getPasswordEncoder());
+//    }
+//
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
