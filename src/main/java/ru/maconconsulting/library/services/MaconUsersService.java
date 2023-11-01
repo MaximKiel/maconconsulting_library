d48e10eb78a76ace67f83951636503857ba01990
MaximKiel <loup.du.nord@yandex.ru>
2023-11-01 20:51:48 +0300

    Implement basic services

--- src/main/java/ru/maconconsulting/library/services/MaconUsersService.java
+++ src/main/java/ru/maconconsulting/library/services/MaconUsersService.java
@@ -2,9 +2,16 @@ package ru.maconconsulting.library.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.maconconsulting.library.models.MaconUser;
 import ru.maconconsulting.library.repositories.MaconUsersRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Transactional(readOnly = true)
 public class MaconUsersService {
 
     private final MaconUsersRepository maconUsersRepository;
@@ -12,5 +19,34 @@ public class MaconUsersService {
     @Autowired
     public MaconUsersService(MaconUsersRepository maconUsersRepository) {
         this.maconUsersRepository = maconUsersRepository;
+    }
+
+    public List<MaconUser> findAll() {
+        return maconUsersRepository.findAll();
+    }
+
+    public Optional<MaconUser> findById(int id) {
+        return maconUsersRepository.findById(id);
+    }
+
+    @Transactional
+    public void save(MaconUser maconUser) {
+        enrichMaconUser(maconUser);
+        maconUsersRepository.save(maconUser);
+    }
+
+    @Transactional
+    public void update(int id, MaconUser updatedMaconUser) {
+        updatedMaconUser.setId(id);
+        maconUsersRepository.save(updatedMaconUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        maconUsersRepository.deleteById(id);
+    }
+
+    private void enrichMaconUser(MaconUser maconUser) {
+        maconUser.setCreatedAt(LocalDateTime.now());
     }
 }
--- src/main/java/ru/maconconsulting/library/services/ProjectsService.java
+++ src/main/java/ru/maconconsulting/library/services/ProjectsService.java
@@ -2,9 +2,16 @@ package ru.maconconsulting.library.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.maconconsulting.library.models.Project;
 import ru.maconconsulting.library.repositories.ProjectsRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Transactional(readOnly = true)
 public class ProjectsService {
 
     private final ProjectsRepository projectsRepository;
@@ -12,5 +19,34 @@ public class ProjectsService {
     @Autowired
     public ProjectsService(ProjectsRepository projectsRepository) {
         this.projectsRepository = projectsRepository;
+    }
+
+    public List<Project> findAll() {
+        return projectsRepository.findAll();
+    }
+
+    public Optional<Project> findById(int id) {
+        return projectsRepository.findById(id);
+    }
+
+    @Transactional
+    public void save(Project project) {
+        enrichProject(project);
+        projectsRepository.save(project);
+    }
+
+    @Transactional
+    public void update(int id, Project updatedProject) {
+        updatedProject.setId(id);
+        projectsRepository.save(updatedProject);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        projectsRepository.deleteById(id);
+    }
+
+    private void enrichProject(Project project) {
+        project.setCreatedAt(LocalDateTime.now());
     }
 }
