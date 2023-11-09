f3429ca2cf8d75a94e68e6db1ab385a767d09b6a
MaximKiel <loup.du.nord@yandex.ru>
2023-11-09 22:00:31 +0300

    Add basic getAllUsers.js

---
+++ src/main/resources/templates/users/getAllUsers.js
@@ -0,0 +1,28 @@
+function showList(){
+    fetch("http://localhost:8080/users")
+        .then(response => response.json())
+        .then(data => createTable(data));
+}
+function createTable(data) {
+    var table = "<table>";
+    // add a row for name and marks
+    table += `<tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                </tr>`;
+    // now loop through students
+    // show their name and marks
+    var tr = "";
+    for(let i = 0; i < data.maconUsers.length; i++) {
+        tr += "<tr>";
+        tr += `<td>${data.maconUsers[i].name}</td>`;
+        tr += `<td>${data.maconUsers[i].email}</td>`;
+        tr += `<td>${data.maconUsers[i].role}</td>`;
+        tr += "</tr>"
+    }
+    table += tr + "</table>";
+
+    // append table to body
+    document.body.innerHTML += table;
+}
