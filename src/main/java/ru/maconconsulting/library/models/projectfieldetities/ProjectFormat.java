249164f4eb94f984d8abbb7eb656c39c0dfafc7c
MaximKiel <loup.du.nord@yandex.ru>
2024-02-02 20:31:15 +0300

    Create empty ProjectFormat.java and ProjectSegment.java

---
+++ src/main/java/ru/maconconsulting/library/models/projectfieldetities/ProjectFormat.java
@@ -0,0 +1,6 @@
+package ru.maconconsulting.library.models.projectfieldetities;
+
+import ru.maconconsulting.library.models.AbstractProjectFieldEntity;
+
+public class ProjectFormat extends AbstractProjectFieldEntity {
+}
---
+++ src/main/java/ru/maconconsulting/library/models/projectfieldetities/ProjectSegment.java
@@ -0,0 +1,6 @@
+package ru.maconconsulting.library.models.projectfieldetities;
+
+import ru.maconconsulting.library.models.AbstractProjectFieldEntity;
+
+public class ProjectSegment extends AbstractProjectFieldEntity {
+}
