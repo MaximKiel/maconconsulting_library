3917039e2be6f8a41082e5fe70c0527346718a10
MaximKiel <loup.du.nord@yandex.ru>
2024-02-05 21:13:14 +0300

    Create empty AbstractProjectFieldsService.java

---
+++ src/main/java/ru/maconconsulting/library/services/projectfields/AbstractProjectFieldsService.java
@@ -0,0 +1,5 @@
+package ru.maconconsulting.library.services.projectfields;
+
+// TODO: implement abstract service with this instructions - https://habr.com/ru/articles/423741/
+public abstract class AbstractProjectFieldsService {
+}
