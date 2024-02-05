596f4a2242f7803d30a6fb39a10c529e3ead27f4
MaximKiel <loup.du.nord@yandex.ru>
2024-02-05 21:16:50 +0300

    Create empty AbstractProjectFieldsController.java

---
+++ src/main/java/ru/maconconsulting/library/controllers/mvc/projectfields/AbstractProjectFieldsController.java
@@ -0,0 +1,5 @@
+package ru.maconconsulting.library.controllers.mvc.projectfields;
+
+// TODO: implement abstract controller with this instructions - https://habr.com/ru/articles/423741/
+public abstract class AbstractProjectFieldsController {
+}
