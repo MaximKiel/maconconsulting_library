0ecf733bc3bf4c5bcde2bdec2f79895f7740b713
MaximKiel <loup.du.nord@yandex.ru>
2024-02-26 09:58:47 +0300

    Create and implement tests for ProjectKeyWordsService

---
+++ src/test/java/ru/maconconsulting/library/util/projectfields/ProjectKeyWordsTestData.java
@@ -0,0 +1,13 @@
+package ru.maconconsulting.library.util.projectfields;
+
+import ru.maconconsulting.library.dto.projectfields.ProjectKeyWordDTO;
+import ru.maconconsulting.library.models.projectfields.ProjectKeyWord;
+
+public class ProjectKeyWordsTestData {
+
+    public static final ProjectKeyWord PROJECT_KEY_WORD_1 = new ProjectKeyWord("Доверительное управление");
+    public static final ProjectKeyWord PROJECT_KEY_WORD_2 = new ProjectKeyWord("Подробная концепция");
+    public static final ProjectKeyWord PROJECT_KEY_WORD_3 = new ProjectKeyWord("Стоимость отделки");
+
+    public static final ProjectKeyWordDTO PROJECT_KEY_WORD_DTO_1 = new ProjectKeyWordDTO("Доверительное управление");
+}
