9b4a0b7e6f3d6ff996fb260656d1eb2d4a41dffd
MaximKiel <loup.du.nord@yandex.ru>
2024-03-04 20:33:09 +0300

    Create and implement PublicationsRepository.java

---
+++ src/main/java/ru/maconconsulting/library/repositories/PublicationsRepository.java
@@ -0,0 +1,13 @@
+package ru.maconconsulting.library.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.maconconsulting.library.models.Publication;
+
+import java.util.Optional;
+
+public interface PublicationsRepository extends JpaRepository<Publication, Integer> {
+
+    Optional<Publication> findByTitle(String title);
+
+    void deleteAllByTitle(String title);
+}
