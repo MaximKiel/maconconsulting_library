abec5e7e7cfaf494e00cb28dd7338524dcf686fc
MaximKiel <loup.du.nord@yandex.ru>
2024-03-05 20:47:33 +0300

    Create PublicationNotFoundException.java

---
+++ src/main/java/ru/maconconsulting/library/utils/exceptions/PublicationNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.maconconsulting.library.utils.exceptions;
+
+public class PublicationNotFoundException extends RuntimeException {
+
+    public PublicationNotFoundException(String message) {
+        super(message);
+    }
+}
