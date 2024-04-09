0071d34873dfcfd1e818ae1b4faed3e8d6ad93e6
MaximKiel <loup.du.nord@yandex.ru>
2024-04-09 20:20:18 +0300

    Delete KeyWordsRepository.java

--- src/main/java/ru/maconconsulting/library/repositories/parameters/KeyWordsRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.maconconsulting.library.repositories.parameters;
-
-import org.springframework.stereotype.Repository;
-import ru.maconconsulting.library.models.parameters.KeyWord;
-
-@Repository
-public interface KeyWordsRepository extends CommonParametersRepository<KeyWord> {
-}
