f09e10525a27918b816bcc2ea65b1f421c6ff5de
MaximKiel <loup.du.nord@yandex.ru>
2024-04-10 20:10:26 +0300

    Delete keyWord util classes

--- src/main/java/ru/maconconsulting/library/utils/exceptions/parameters/KeyWordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.maconconsulting.library.utils.exceptions.parameters;
-
-public class KeyWordNotFoundException extends RuntimeException {
-    public KeyWordNotFoundException(String message) {
-        super(message);
-    }
-}
