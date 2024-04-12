6b95b0ffb818ec3724ed1b88ba1c8ef623c1c533
MaximKiel <loup.du.nord@yandex.ru>
2024-04-12 08:31:34 +0300

    Replace type with chapter in utils

---
+++ src/main/java/ru/maconconsulting/library/utils/exceptions/parameters/ChapterNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.maconconsulting.library.utils.exceptions.parameters;
+
+public class ChapterNotFoundException extends RuntimeException {
+    public ChapterNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/maconconsulting/library/utils/exceptions/parameters/TypeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.maconconsulting.library.utils.exceptions.parameters;
-
-public class TypeNotFoundException extends RuntimeException {
-    public TypeNotFoundException(String message) {
-        super(message);
-    }
-}
