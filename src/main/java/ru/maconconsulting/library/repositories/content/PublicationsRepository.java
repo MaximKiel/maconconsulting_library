da14ae396aff3fabdd2c4fdd31267b7f5b56e000
MaximKiel <loup.du.nord@yandex.ru>
2024-07-01 21:24:39 +0300

    Add repository annotation into PublicationsRepository.java

--- src/main/java/ru/maconconsulting/library/repositories/content/PublicationsRepository.java
+++ src/main/java/ru/maconconsulting/library/repositories/content/PublicationsRepository.java
@@ -1,10 +1,12 @@
 package ru.maconconsulting.library.repositories.content;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.maconconsulting.library.models.content.Publication;
 
 import java.util.Optional;
 
+@Repository
 public interface PublicationsRepository extends JpaRepository<Publication, Integer> {
 
     Optional<Publication> findByTitle(String title);
