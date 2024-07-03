967225d1f9c54f8769c20cc056aac7a4374dcd7f
MaximKiel <loup.du.nord@yandex.ru>
2024-07-03 19:35:07 +0300

    Refactor PublicationsService.java

--- src/main/java/ru/maconconsulting/library/services/content/PublicationsService.java
+++ src/main/java/ru/maconconsulting/library/services/content/PublicationsService.java
@@ -69,23 +69,23 @@ public class PublicationsService {
 
     public List<Publication> search(SearchPublication searchPublication) {
         List<Publication> result = findAll().stream().sorted(Comparator.comparing(Publication::getTitle)).collect(Collectors.toList());
-        if (!searchPublication.getTitle().trim().equals("")) {
+        if (searchPublication.getTitle() != null && !searchPublication.getTitle().trim().equals("")) {
             result = searchElement(result, p -> p.getTitle().toLowerCase().contains(searchPublication.getTitle().trim().toLowerCase()));
         }
-        if (!searchPublication.getAnnotation().trim().equals("")) {
+        if (searchPublication.getAnnotation() != null && !searchPublication.getAnnotation().trim().equals("")) {
             result = searchElement(result, p -> p.getAnnotation() != null && !p.getAnnotation().equals("") && p.getAnnotation().toLowerCase().contains(searchPublication.getAnnotation().trim().toLowerCase()));
         }
-        if (!searchPublication.getSource().trim().equals("")) {
+        if (searchPublication.getSource() != null && !searchPublication.getSource().trim().equals("")) {
             result = searchElement(result, p -> p.getSource() != null && !p.getSource().equals("") && p.getSource().toLowerCase().contains(searchPublication.getSource().trim().toLowerCase()));
         }
-        if (searchPublication.getYear() != null && searchPublication.getYear() != 0) {
+        if (searchPublication.getYear() != 0) {
             result = searchElement(result, p -> p.getYear() != null && p.getYear().equals(searchPublication.getYear()));
         }
-        if (!searchPublication.getRelevance().trim().equals("")) {
+        if (searchPublication.getRelevance() != null && !searchPublication.getRelevance().trim().equals("")) {
             result = searchElement(result, p -> p.getRelevance() != null && !p.getRelevance().equals("") && p.getRelevance().toLowerCase().contains(searchPublication.getRelevance().trim().toLowerCase()));
         }
-        if (!searchPublication.getLocation().trim().equals("")) {
-            result = searchElement(result, p -> p.getLocation().toLowerCase().contains(searchPublication.getLocation().trim().toLowerCase()));
+        if (searchPublication.getLocation() != null && !searchPublication.getLocation().trim().equals("")) {
+            result = searchElement(result, p -> p.getLocation() != null && !p.getLocation().equals("") && p.getLocation().toLowerCase().contains(searchPublication.getLocation().trim().toLowerCase()));
         }
         if (searchPublication.getSegment() != null && !searchPublication.getSegment().getName().equals("")) {
             result = searchElement(result, p -> {
@@ -99,8 +99,8 @@ public class PublicationsService {
                 return formatNames.stream().anyMatch(n -> n.equals(searchPublication.getFormat().getName()));
             });
         }
-        if (!searchPublication.getKeyWord().trim().equals("")) {
-            result = searchElement(result, p -> p.getKeyWords().toLowerCase().contains(searchPublication.getKeyWord().trim().toLowerCase()));
+        if (searchPublication.getKeyWord() != null && !searchPublication.getKeyWord().trim().equals("")) {
+            result = searchElement(result, p -> p.getKeyWords() != null && !p.getKeyWords().equals("") && p.getKeyWords().toLowerCase().contains(searchPublication.getKeyWord().trim().toLowerCase()));
         }
         return result;
     }
@@ -110,18 +110,18 @@ public class PublicationsService {
     }
 
     private Set<Segment> enrichSegments(SegmentsService service, Publication publication) {
-        Set<Segment> segments = new HashSet<>();
+        Set<Segment> entities = new HashSet<>();
         for (Segment s : publication.getSegments()) {
-            segments.add(service.findByName(s.getName()).orElseThrow());
+            entities.add(service.findByName(s.getName()).orElseThrow());
         }
-        return segments;
+        return entities;
     }
 
     private Set<Format> enrichFormats(FormatsService service, Publication publication) {
-        Set<Format> formats = new HashSet<>();
+        Set<Format> entities = new HashSet<>();
         for (Format f : publication.getFormats()) {
-            formats.add(service.findByName(f.getName()).orElseThrow());
+            entities.add(service.findByName(f.getName()).orElseThrow());
         }
-        return formats;
+        return entities;
     }
 }
