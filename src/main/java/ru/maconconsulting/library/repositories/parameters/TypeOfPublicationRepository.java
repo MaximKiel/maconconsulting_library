f5f56af4f5595973d3cca6d8e86b8854bdf4322c
MaximKiel <loup.du.nord@yandex.ru>
2024-07-30 19:12:58 +0300

    Create repository for new parameter TypeOfPublication

---
+++ src/main/java/ru/maconconsulting/library/repositories/parameters/TypeOfPublicationRepository.java
@@ -0,0 +1,8 @@
+package ru.maconconsulting.library.repositories.parameters;
+
+import org.springframework.stereotype.Repository;
+import ru.maconconsulting.library.models.parameters.TypeOfPublication;
+
+@Repository
+public interface TypeOfPublicationRepository extends CommonParametersRepository<TypeOfPublication> {
+}
